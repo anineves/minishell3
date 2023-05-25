69c9c8b14e15d1c805d8ac8faae5b1e6cc8717cc
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2023-05-26 00:10:22 +0100

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,5 +1,3 @@
-#include "minishell.h"
-
 void execute(t_data *shell)
 {
 	if (!shell->args[0])
@@ -8,17 +6,8 @@ void execute(t_data *shell)
 		ft_echo(shell->args);
 	else if (ft_strncmp(shell->args[0], "pwd", 3) == 0)
 		ft_pwd();
-	//else if (ft_strcmp(shell->args[0], "cd") == 0)
-	//	cd(att);
-	//else if (ft_strcmp(shell->args[0], "export") == 0)
-	//	export(att);
-	//else if (ft_strcmp(shell->args[0], "unset") == 0)
-	//	unset(att);
-	//else if (ft_strcmp(shell->args[0], "env") == 0)
-	//	env(att);
-	//else if ((ft_strcmp(shell->args[0], "exit") == 0) || (ft_strcmp(tok[0], "\"exit\"") == 0))
-	//	return (ft_exit(*att));
-	//else
-		//execute(att);
+	
+	
+	
 	return;
 }
